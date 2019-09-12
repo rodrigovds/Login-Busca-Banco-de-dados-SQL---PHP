c3b11f529058f08fd73bc1988f8d68ce9f1fbc6b
Rodrigo Mondini <47304763+rodrigovds@users.noreply.github.com>
2019-09-12 19:20:43 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Arquivando</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+<style>
+
+img {
+  border-radius: 8px;
+  
+  
+}
+
+  a:link, a:visited {
+  background-color: green;
+  color: white;
+  padding: 15px 35px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  box-shadow: 2px 2px 4px #000000;
+
+  
+  
+}
+div{
+
+padding: 30px;
+
+}
+#conteudo{
+
+padding: 30px;
+    
+}
+</style>
+      
+</head>
+
+
+
+<body>
+
+
+    <div class="menu-top" align="center">
+        <a href="index.php" target="_self"><b>Página Inicial</b></a>
+        <a href="login.php" target="_self"><b>Login</b></a>
+        <a href="cadastrar.html" target="_self"><b>Cadastrar</b></a>
+        <a href="cadastro.php" target="_self"><b>Cadastros</b></a>
+        <a href="gerenciando.php" target="_self"><b>Gerenciando</b></a>
+      </div>
+
+
+
+
+    <div class="menu-top" align="center">
+    <p>Bem Vindo, fique a vontade!! </p>
+       Para mais acesso, faça login.<br><br>
+    
+    <img src="documentosvideos.jpeg" alt="inicio" width="250" height="250">
+</div>
+
+
+
+
+
+</body>
+</html>
