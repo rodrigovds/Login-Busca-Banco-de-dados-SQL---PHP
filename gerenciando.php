fd3e30f690466bd1f3469b7023a1e22a693ad542
Rodrigo Mondini <47304763+rodrigovds@users.noreply.github.com>
2019-09-12 19:23:49 -0300

    Create gerenciando.php

---
+++ gerenciando.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Arquivando</title>
+     
+        
+        
+        <style>
+        
+        
+            a:link, a:visited {
+   background-color: green;
+   color: white;
+   padding: 15px 35px !important;
+   text-align: center;
+   text-decoration: none;
+   display: inline-block;
+   box-shadow: 2px 2px 4px #000000;
+            }
+            div{
+
+
+padding: 30px  ;
+
+
+
+}   
+   </style>
+ </head>
+ 
+ 
+ <body>
+ 
+
+  
+
+    <div class="menu-top" align="center">
+        <a href="index.php" target="_self"><b>Página Inicial</b></a>
+        <a href="login.php" target="_self"><b>Login</b></a>
+        <a href="cadastrar.html" target="_self"><b>Cadastrar</b></a>
+        <a href="cadastro.php" target="_self"><b>Cadastros</b></a>
+        <a href="gerenciando.php" target="_self"><b>Gerenciando</b></a>
+      </div>
+
+
+<?php
+session_start();
+
+
+
+?>
+
+
+<h2 align="center"><blink> Espera ai que aqui vai te surpresa! <br>
+
+<p> Pagina para ver os arquivos: </p>
+
+Verificar login; (Dado E-mail)<br>
+
+Perguntar qual tipo de documento, se quer ver ou tudo, ou buscar por tipo, adicionar novo.<br>
+
+<p>Para adicionar novo:</p>
+
+Selecionar Arquivo - tipo de arquivo (Dar opções para escolher (Para qual finalidade o documento) - <br>Nome do Arquivo  (sempre aparecer em miniatura) se clicar abre modal com a imagem  </blink> </h2>
+
+
+
+
+</body>
+</html>
